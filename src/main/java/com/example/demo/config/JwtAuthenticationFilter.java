6ed64d460f84e266516d776b83fd50896922d52e
Adeniyi-Bella <adeniyibella@gmail.com>
2023-04-15 12:10:39 +0200

    check if user has been authenticated and generates jwt token for user

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -6,8 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 
 import java.io.IOException;
 
@@ -16,17 +23,22 @@ import java.io.IOException;
 // create a constructor using any final field
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    // from userdetails class
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        // extract header from request body
+        // extract header
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        // Beare token starts with Bearer 
+        // Bearer token starts with Bearer
         if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
             // pass to the next filter
             filterChain.doFilter(request, response);
@@ -34,9 +46,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         // count from after Bearer
         jwt = authHeader.substring(7);
-/*
+        // create a service class that extracts the email from the request body
         userEmail = jwtService.extractUsername(jwt);
-*/
+    // check if user is available or user has been validated once before expiration
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // already implemented in Application.java which matches the email in our DB
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
+            /*var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);*/
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
     }
 }
